b2ace06c6c3a825bfc7dc2856472f5c05602f440
smallboyintech <111036308+tabpaddy@users.noreply.github.com>
2024-05-30 22:24:55 +0100

    Add files via upload

---
+++ config/constants.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+define('ROOT_URL', 'http://localhost:8080/blog_app/');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'blog');
+
+?>
